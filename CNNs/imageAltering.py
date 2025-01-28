a6d7cc0ca7cbfae17b174375318caf1378980f9b
ziyad <ziyad.alnawfal1@gmail.com>
2025-01-28 16:48:28 +0300

    removed redudant

--- CNNs/imageAltering.py
+++ CNNs/imageAltering.py
@@ -11,16 +11,8 @@ for i in range(0,100):
     dracula.putpixel((x,y),(0,0,0))
 
 dracula.show()
-from PIL import Image
-import random
 
 dracula = Image.open(r"C:/Users/Ziyad/Downloads/draculaCopy.png")
 pixels = dracula.load()
-
-for i in range(0, 100):
-    x = random.randint(0, dracula.width - 1)
-    y = random.randint(0, dracula.height - 1)
-    print(x, y)
-    dracula.putpixel((x, y), (0, 0, 0))
 
 dracula.save(r"C:/Users/Ziyad/Downloads/draculaAltered.png")
